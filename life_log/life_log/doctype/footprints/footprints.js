3f94df064a524e73717a929428b9aa66af342335
Sanjay Raja <thesanjayraja@gmail.com>
2025-01-31 01:15:48 +0530

    Initial Commit

---
+++ life_log/life_log/doctype/footprints/footprints.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sanjay Raja S and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Footprints", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ life_log/life_log/doctype/footprints/test_footprints.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Sanjay Raja S and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFootprintsLocation(FrappeTestCase):
+	pass
