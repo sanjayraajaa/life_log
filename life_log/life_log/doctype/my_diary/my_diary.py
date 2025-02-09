e9d90c26bf78e481d076eed96a10932b58fd4c55
Sanjay Raja <thesanjayraja@gmail.com>
2025-02-09 22:37:45 +0530

    Docname user Specified Updated

--- life_log/life_log/doctype/my_diary/my_diary.py
+++ life_log/life_log/doctype/my_diary/my_diary.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025, Sanjay Raja S and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MyDiary(Document):
-	pass
+    
+    def autoname(self):
+        user_email = frappe.session.user_email
+        date = frappe.utils.today()  # Get current date
+        self.name = f"{date}-{self.title}__{user_email}"
